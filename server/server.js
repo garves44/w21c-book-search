a63bc915f6c2dab5156b542ae886d40cf0e76a1a
garves44 <garves44@gmail.com>
2020-09-24 11:15:57 -0600

    updated server to use ApolloServer and not RestAPI route

--- server/server.js
+++ server/server.js
@@ -1,12 +1,26 @@
+//Dependencies
 const express = require("express");
 const path = require("path");
 const db = require("./config/connection");
-const routes = require("./routes");
-const { typeDefs, resolvers } = require("./schemas");
-
 const app = express();
+
+//ApolloServer Dependencies
+const { ApolloServer } = require("apollo-server-express");
+const { typeDefs, resolvers } = require("./schemas");
+const { authMiddleware } = require("./utils/auth");
+
+// New ApolloServer with Schema and authMiddleware passed in
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: authMiddleware,
+});
+
 const PORT = process.env.PORT || 3001;
 
+//New ApolloServer with middleware
+server.applyMiddleware({ app });
+//Express middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
@@ -14,9 +28,16 @@ app.use(express.json());
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../client/build")));
 }
-
-app.use(routes);
+//Wild Card Path
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/build/index"));
+});
 
 db.once("open", () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+  app.listen(PORT, () => {
+    console.log(`🌍 Now listening on localhost:${PORT}`);
+    console.log(
+      `To use gql go to http://localhost:${PORT}${server.graphqlPath}`
+    );
+  });
 });
