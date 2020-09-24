3d2a8cd1d2c04f1ee0b1dfb291937621a8509af4
garves44 <garves44@gmail.com>
2020-09-24 10:53:51 -0600

    added a gql querie to typeDefss for User, Query, Book, Auth, Mutation, and savedBook

--- server/schemas/resovlers.js
+++ server/schemas/resovlers.js
@@ -4,9 +4,11 @@ const { User } = require("../models");
 
 const resolvers = {
   Query: {
-    me: async (parent, args, context) => {
-      if (context.user) {
-        const data = await User.findOne({ _id: context.user._id }).select(
+    me: async (parent, args, { user }) => {
+      // user is destructured out of context
+      if (user) {
+        //get user data into a variable removes password
+        const data = await User.findOne({ _id: user._id }).select(
           "-__ -password"
         );
         return data;
@@ -16,14 +18,18 @@ const resolvers = {
   },
 
   Mutation: {
+    //Mutation for login takes parent {email, password} are destructured out of args
     login: async (parent, { email, password }) => {
       const user = await User.findOne({ email });
+      // true/false variable if password is correct
       const correctPw = await user.isCorrectPassword(password);
       const token = signToken(user);
 
+      //if user does not exist throws an error
       if (!user) {
         throw new AuthenticationError("Wrong Username or Password!");
       }
+      //if password is incorrect throws an error
       if (!correctPw) {
         throw new AuthenticationError("Wrong Username or Password");
       }
---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,49 @@
+// Dependencies
+const gql = require("apollo-server-express");
+
+//need User, Query, Book, Auth, Mutation, & savedBook
+const typeDefs = gql`
+  type User {
+    _id: ID
+    username: String
+    email: String
+    bookCount: Int
+    savedBooks: [Book]
+  }
+
+  type Query {
+    me: User
+  }
+
+  type Book {
+    bookId: ID
+    authors: [String]
+    description: String
+    title: String
+    image: String
+    link: String
+  }
+
+  type Auth {
+    token: ID!
+    user: User
+  }
+
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    saveBook(input: savedBook!): User
+    removeBook(bookId: String!): User
+  }
+
+  type savedBook {
+    bookId: String
+    title: String
+    authors: [String]
+    description: String
+    image: String
+    link: String
+  }
+`;
+
+module.exprots = typeDefs;
