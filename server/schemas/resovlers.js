cbc08071ec655276bba20d1d8cafe99057759c57
garves44 <garves44@gmail.com>
2020-09-24 11:05:04 -0600

    added saveBook, removeBook and addUser to mutations in resovlers.js

--- server/schemas/resovlers.js
+++ server/schemas/resovlers.js
@@ -1,6 +1,7 @@
 //Dependencies
 const { AuthenticationError } = require("apollo-server-express");
 const { User } = require("../models");
+const { signToken } = require("../utils/auth");
 
 const resolvers = {
   Query: {
@@ -35,5 +36,44 @@ const resolvers = {
       }
       return { user, token };
     },
+
+    //addUser takes parents and args
+    addUser: async (parent, args) => {
+      //create a user with what ever args are passed
+      const user = await User.create(args);
+      //sign the token with the user info
+      const token = signToken(user);
+
+      return { user, token };
+    },
+
+    //saveBook takes parent, {input} destructured from args, {user} destructured from context
+    saveBook: async (parent, { input }, { user }) => {
+      //if logged in will update the saved books for the user
+      if (user) {
+        const saveUserBooks = await User.findByIdAndUpdate(
+          { _id: user._id },
+          { $push: { savedBooks: input } },
+          { new: true, runValidators: true }
+        );
+        return saveUserBooks;
+      }
+      throw new AuthenticationError("Login Please");
+    },
+
+    //removeBook takes parent, {bookId} destructured from args, {user} destructured from context
+    removeBook: async (parent, { bookId }, { user }) => {
+      //if logged in will remove book from saved list of books for user
+      const saveUserBooks = await User.findByIdAndDelete(
+        { _id: user._id },
+        {
+          $pull: { savedBooks: { bookId: bookId } },
+        },
+        { new: true }
+      );
+      return saveUserBooks;
+    },
   },
 };
+
+module.exports = resolvers;
