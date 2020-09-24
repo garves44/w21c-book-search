74f6235103d1fc2d4d8a47ef3a37952ebc309070
garves44 <garves44@gmail.com>
2020-09-24 11:38:58 -0600

    created Apollo Provider in App.js to make requests work with Apollo Server

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,43 @@
-import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import SearchBooks from './pages/SearchBooks';
-import SavedBooks from './pages/SavedBooks';
-import Navbar from './components/Navbar';
+//Dependencies
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import SearchBooks from "./pages/SearchBooks";
+import SavedBooks from "./pages/SavedBooks";
+import Navbar from "./components/Navbar";
+import { ApolloProvider } from "@apollo/react-hooks";
+import ApolloClient from "apollo-boost";
+
+//put new ApolloClient into a client variable
+const client = new ApolloClient({
+  request: (operation) => {
+    //token is pulled from localStorage
+    const token = localStorage.getItem("id_token");
+
+    operation.setContext({
+      headers: {
+        // if the token is there set Bearer 'token' else ''
+        authorization: token ? `Bearer ${token}` : "",
+      },
+    });
+  },
+  uri: "/graphql",
+});
 
 function App() {
   return (
-    <Router>
-      <>
-        <Navbar />
-        <Switch>
-          <Route exact path='/' component={SearchBooks} />
-          <Route exact path='/saved' component={SavedBooks} />
-          <Route render={() => <h1 className='display-2'>Wrong page!</h1>} />
-        </Switch>
-      </>
-    </Router>
+    //wrap entire App with ApolloProvider
+    <ApolloProvider>
+      <Router>
+        <>
+          <Navbar />
+          <Switch>
+            <Route exact path="/" component={SearchBooks} />
+            <Route exact path="/saved" component={SavedBooks} />
+            <Route render={() => <h1 className="display-2">Wrong page!</h1>} />
+          </Switch>
+        </>
+      </Router>
+    </ApolloProvider>
   );
 }
 
